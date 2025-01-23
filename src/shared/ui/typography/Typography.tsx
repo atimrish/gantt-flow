c72337657b6d185cc477d3d1dc6f3ee97d28b2f0
atimrish <atimrish11@gmail.com>
2025-01-23 19:23:30 +0400

    [fix] классы в компоненте Typography

--- src/shared/ui/typography/Typography.tsx
+++ src/shared/ui/typography/Typography.tsx
@@ -10,31 +10,51 @@ type TextProps = {
 }
 
 const Heading = (p: HeadingProps & HTMLAttributes<HTMLHeadingElement>) => {
+    let classNames = s.heading
+    if (p.className) {
+        classNames += ' ' + p.className
+    }
+
     const elems: Record<NonNullable<HeadingProps["as"]>, ReactNode> =  {
-        'h1': <h1 {...p} className={s.heading + ' ' + p.className}>{p.children}</h1>,
-        'h2': <h2 {...p} className={s.heading + ' ' + p.className}>{p.children}</h2>,
-        'h3': <h3 {...p} className={s.heading + ' ' + p.className}>{p.children}</h3>,
-        'h4': <h4 {...p} className={s.heading + ' ' + p.className}>{p.children}</h4>,
-        'h5': <h5 {...p} className={s.heading + ' ' + p.className}>{p.children}</h5>,
-        'h6': <h6 {...p} className={s.heading + ' ' + p.className}>{p.children}</h6>,
+        'h1': <h1 {...p} className={classNames}>{p.children}</h1>,
+        'h2': <h2 {...p} className={classNames}>{p.children}</h2>,
+        'h3': <h3 {...p} className={classNames}>{p.children}</h3>,
+        'h4': <h4 {...p} className={classNames}>{p.children}</h4>,
+        'h5': <h5 {...p} className={classNames}>{p.children}</h5>,
+        'h6': <h6 {...p} className={classNames}>{p.children}</h6>,
     }
     return elems[p.as || 'h2']
 }
 
 const Paragraph = (p: HTMLAttributes<HTMLParagraphElement>) => {
-    return <p {...p} className={s.text + ' ' + p.className}>{p.children}</p>
+    let classNames = s.text
+    if (p.className) {
+        classNames += ' ' + p.className
+    }
+
+    return <p {...p} className={classNames}>{p.children}</p>
 }
 
 const Text = (p: TextProps & HTMLAttributes<HTMLElement>) => {
+    let classNames = s.text
+    if (p.className) {
+        classNames += ' ' + p.className
+    }
+
     const elems: Record<NonNullable<TextProps["as"]>, ReactNode> =  {
-        'div': <div {...p} className={s.text + ' ' + p.className}>{p.children}</div>,
-        'span': <span {...p} className={s.text + ' ' + p.className}>{p.children}</span>,
+        'div': <div {...p} className={classNames}>{p.children}</div>,
+        'span': <span {...p} className={classNames}>{p.children}</span>,
     }
     return elems[p.as || 'div']
 }
 
 const Link = (p: HTMLAttributes<HTMLAnchorElement>) => {
-    return <a {...p} className={s.text + ' ' + p.className}>{p.children}</a>
+    let classNames = s.text
+    if (p.className) {
+        classNames += ' ' + p.className
+    }
+
+    return <a {...p} className={classNames}>{p.children}</a>
 }
 
 export const Typography = {Heading, Text, Paragraph, Link}
